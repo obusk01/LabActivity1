98136411dd45db065c54594bc961a9e3fe2e9feb
Olivia Busk <123026517+obusk01@users.noreply.github.com>
2023-01-25 15:57:04 -0500

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,90 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# The Authors' names are Bethany Berlage and Olivia Busk
+print("Hello, World")
+Hello, World
+print("Welcome to CPSC 207!")
+Welcome to CPSC 207!
+print("Hello\nWorld")
+Hello
+World
+print("How\nAre\nYou?")
+How
+Are
+You?
+print("Today", "is", "Wednesday")
+Today is Wednesday
+print("Today" + "is"+ "Wednesday")
+TodayisWednesday
+print("a\nb\nc")
+a
+b
+c
+print("Hello" + "World")
+HelloWorld
+print("Hello\n" + "\nWorld!")
+Hello
+
+World!
+print("123\n45\n6)
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print("123\n45\n6")
+      
+123
+45
+6
+1+2*3
+      
+7
+(1+2)*3
+      
+9
+7/3
+      
+2.3333333333333335
+7//3
+      
+2
+7%3
+      
+1
+22/4
+      
+5.5
+22//4
+      
+5
+22%4
+      
+2
+3**2
+      
+9
+2**3
+      
+8
+(4+3)//2
+      
+3
+6%2
+      
+0
+1+2*3**4
+      
+163
+print("2+2=", 2+2)
+      
+2+2= 4
+print("50 pounds is
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print("50 pounds is ", 50*.453592, "Kilograms")
+      
+50 pounds is  22.6796 Kilograms
+
+50 pounds is  22.6796 Kilograms
+      
+SyntaxError: invalid syntax
+#The Authors' are Bethany Berlage. Saerin Bong, and Olivia Busk
+      
